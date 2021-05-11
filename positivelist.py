779a6b6d774dae6fe642722aff4603a0da101ab7
VijayaPrashanth <83896990+VijayaPrashanth@users.noreply.github.com>
2021-05-11 18:24:02 +0530

    Create positivelist.py

---
+++ positivelist.py
@@ -0,0 +1,20 @@
+list1=[]
+n=int(input("enter the no.of.elements for list1 : "))
+for i in range(0,n):
+  e=input()
+  list1.append(e)
+print(list1)
+for j in list1:
+  g=int(j)
+  if g>0 :
+    print(g)
+list2=[]
+m=int(input("enter the no.of.elements for list2 : "))
+for k in range(0,m):
+  f=input()
+  list2.append(f)
+print(list2)
+for l in list2 :
+  h=int(l)
+  if h>0:
+    print(h)
